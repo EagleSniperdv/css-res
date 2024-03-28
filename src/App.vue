361739bbb1dfc7087e62a161f3a8531588bc08c5
EagleSniperdv <106923143+EagleSniperdv@users.noreply.github.com>
2024-03-28 16:53:50 +0300

    flex

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import FlexBox from './components/FlexBox.vue'
+</script>
+
+<template>
+  
+  <FlexBox msg="FlexBox + Vue" />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/FlexBox.vue
@@ -0,0 +1,43 @@
+<script setup>
+
+defineProps({
+  msg: String,
+})
+
+</script>
+
+<template>
+
+
+  <h1>{{ msg }}</h1>
+  
+  <section id="Parent">
+    
+    <h2>props of parent flex cont</h2>
+    <div class="flexbox-container">
+      <div class="flexbox_item">item-1</div>
+      <div class="flexbox_item">item-2</div>
+      <div class="flexbox_item">item-3</div>
+      <div class="flexbox_item">item-4</div>
+      <div class="flexbox_item">item-5</div>
+      <div class="flexbox_item">item-6</div>
+    </div>
+  </section>
+  <section id="children">
+    <h2>props of children flex cont</h2>
+    <div class="flexbox-container">
+      <div class="flexbox_item">item-1</div>
+      <div class="flexbox_item">item-2</div>
+      <div class="flexbox_item">item-3</div>
+      <div class="flexbox_item">item-4</div>
+      <div class="flexbox_item">item-5</div>
+      <div class="flexbox_item">item-6</div>
+    </div>
+  </section>
+
+  
+</template>
+
+<style scoped>
+  
+</style>
