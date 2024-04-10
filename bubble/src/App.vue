8e0eadafc77c96b6b089cf4b364a5c68c7aafd78
EagleSniperdv <106923143+EagleSniperdv@users.noreply.github.com>
2024-04-10 13:30:27 +0300

    bubble website

---
+++ bubble/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+<main class="class-container">
+  <nav id="nav">
+    <img src="./img/25068-6-bubbles-file.png" alt="logo" id="logo">
+    <label for="logo">Bubbling</label>
+    <button class="btn hover">sign-up</button>
+  </nav>
+</main>
+</template>
+
+<style scoped>
+
+</style>
