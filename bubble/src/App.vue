1ade1f68c8545aa4965ba9db20aaa2428c344e38
EagleSniperdv <106923143+EagleSniperdv@users.noreply.github.com>
2024-04-27 12:44:27 +0300

    2

--- bubble/src/App.vue
+++ bubble/src/App.vue
@@ -3,15 +3,28 @@
 </script>
 
 <template>
-<main class="class-container">
+<main class="main-container">
   <nav id="nav">
-    <img src="./img/25068-6-bubbles-file.png" alt="logo" id="logo">
-    <label for="logo">Bubbling</label>
+    <!-- <img src="./img/25068-6-bubbles-file.png" alt="logo" id="logo">
+    <label for="logo">Bubbling</label> -->
+    <button class="btn hover">sign-up</button>
+    <button class="btn hover">sign-up</button>
+    <button class="btn hover">sign-up</button>
     <button class="btn hover">sign-up</button>
   </nav>
 </main>
 </template>
 
 <style scoped>
+.main-container {
+    width: 100%;
+    height: 100%;
+    
+  }
 
+  button{
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 111px;
+  }
 </style>
